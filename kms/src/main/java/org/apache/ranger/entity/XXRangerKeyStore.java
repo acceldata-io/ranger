683daa2f260d274fe06b4146261e677c57109b04
Dhaval Shah <dhavalshah9131@gmail.com>
2024-04-17 15:49:19 +0530

    ODP-3326: RANGER-4775 : Ranger Kms is failing with oracle23 Database
    (cherry picked from commit 90c8ebb72b5ad52e6f705e7e88e12c711c19c97e)

--- kms/src/main/java/org/apache/ranger/entity/XXRangerKeyStore.java
+++ kms/src/main/java/org/apache/ranger/entity/XXRangerKeyStore.java
@@ -23,7 +23,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Lob;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -72,7 +71,6 @@ public class XXRangerKeyStore extends XXDBBase implements java.io.Serializable {
 		this.createdDate = createdDate;
 	}
 
-	@Lob
 	@Column(name="kms_encoded")
 	protected String encoded;
 
