5662b093c63885e94512afc608607b06200780ae
Prabhjyot Singh <prabhjyot@acceldata.io>
2024-05-03 11:21:01 -0400

    ODP-1404: RANGER-3409: Updated jackson library

--- agents-common/src/main/java/org/apache/ranger/plugin/util/ServiceTags.java
+++ agents-common/src/main/java/org/apache/ranger/plugin/util/ServiceTags.java
@@ -20,6 +20,7 @@
 package org.apache.ranger.plugin.util;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
@@ -35,11 +36,10 @@ import javax.xml.bind.annotation.XmlRootElement;
 import org.apache.ranger.plugin.model.RangerServiceResource;
 import org.apache.ranger.plugin.model.RangerTag;
 import org.apache.ranger.plugin.model.RangerTagDef;
-import org.codehaus.jackson.annotate.JsonAutoDetect;
-import org.codehaus.jackson.annotate.JsonIgnore;
-import org.codehaus.jackson.annotate.JsonIgnoreProperties;
-import org.codehaus.jackson.annotate.JsonAutoDetect.Visibility;
-import org.codehaus.jackson.map.annotate.JsonSerialize;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 @JsonAutoDetect(fieldVisibility=Visibility.ANY)
 @JsonSerialize(include=JsonSerialize.Inclusion.NON_NULL)
