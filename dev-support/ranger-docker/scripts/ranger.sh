afe458a881c9cd7834b5e62fbbbf0f7d13d1fa62
Dishti Kundra <136782345+dishtikundra@users.noreply.github.com>
2025-02-24 19:25:43 +0530

    ODP-2718|ODP-2746 Refactor shebangs for python scripts to ambari-python-wrap (#61)
    * ODP-2746 : Refactor shebangs for python scripts to ambari-python-wrap
    
    * ODP-2746 : Refactor shebangs for python scripts to ambari-python-wrap
    
    * ODP-3394 - RANGER-3795: Fix java patch J10033 and J10046 failure
    
    (cherry picked from commit 81b928c8956e2f8d5bf68d451c74f83abc01c48a)
    
    * ODP-3401 - RANGER-3853: Persist db updates immediately using Transaction Management in J10054
    
    Signed-off-by: pradeep <pradeep@apache.org>
    (cherry picked from commit c4ee5a860424e335025b7eaa261c2289c9c93301)
    
    * ODP-3402 - RANGER-3854: Persist db updates immediately using Transaction Management in J10056
    
    Signed-off-by: pradeep <pradeep@apache.org>
    (cherry picked from commit 1671b142aed7b7f37257a94f71ea0bce6708b709)
    
    * ODP-3403 - RANGER-3857: Fix Ranger java patch J10055 performance issue
    
    * ODP-3358 - RANGER-3906: Ranger KMS unable to start with Oracle 19 database
    
    * ODP-3395 - RANGER-4915:Fix the default SSL Ciphers are too weak issue for user sync service (#375)
    
    (cherry picked from commit e671f8614459e6e12d4447c55196d2037cf52e7c)
    
    * ODP-3397 - Removing duplicate dependencies & missing jackson-core issue on tagsync
    
    ---------
    
    Co-authored-by: harshith gandhe <harshith@acceldata.io>
    Co-authored-by: pradeep <pradeep@apache.org>
    Co-authored-by: Abhishek Kumar <abhishekkumar100031@gmail.com>
    Co-authored-by: Shubham Sharma <shubh.luck@yahoo.in>
    Co-authored-by: zhenye zhang <zhangzhenye_1983@163.com>

--- dev-support/ranger-docker/scripts/ranger.sh
+++ dev-support/ranger-docker/scripts/ranger.sh
@@ -41,7 +41,7 @@ if [ "${SETUP_RANGER}" == "true" ]
 then
   # Wait for Ranger Admin to become ready
   sleep 30
-  python3 ${RANGER_SCRIPTS}/create-ranger-services.py
+  ambari-python-wrap ${RANGER_SCRIPTS}/create-ranger-services.py
 fi
 
 RANGER_ADMIN_PID=`ps -ef  | grep -v grep | grep -i "org.apache.ranger.server.tomcat.EmbeddedServer" | awk '{print $2}'`
