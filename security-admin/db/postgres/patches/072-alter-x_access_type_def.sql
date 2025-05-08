a481692c96ceeee1fc9717dc3e78834cfe1b6481
Prabhjyot Singh <prabhjyotsingh@gmail.com>
2025-05-08 19:57:41 -0400

    ODP-4248: Migrate old 3.0.0 to 2.5.0
    (cherry picked from commit 3d5baaf57b59963a5f23fa9ad33c44a040a60126)
    (cherry picked from commit f7611ddf8699b94b2e4fe00d1d5a34b77df1fad9)

--- security-admin/db/postgres/patches/072-alter-x_access_type_def.sql
+++ security-admin/db/postgres/patches/072-alter-x_access_type_def.sql
@@ -34,6 +34,9 @@ select add_category_x_access_type_def();
 select 'delimiter end';
 commit;
 
+-- todo:Prabhu remove this once we are officially on 3.0.0
+update x_db_version_h set inst_by='Ranger 2.5.0' where inst_by = 'Ranger 3.0.0';
+
 
 
 
