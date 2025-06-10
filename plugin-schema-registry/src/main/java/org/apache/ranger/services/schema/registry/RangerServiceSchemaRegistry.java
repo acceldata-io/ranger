ab25f1ebfeb3fe9ce8f74b0c97ce572f1c7a75e8
Shubham Sharma <shubh.luck@yahoo.in>
2025-06-10 09:44:23 -0400

    Removed import comment

--- plugin-schema-registry/src/main/java/org/apache/ranger/services/schema/registry/RangerServiceSchemaRegistry.java
+++ plugin-schema-registry/src/main/java/org/apache/ranger/services/schema/registry/RangerServiceSchemaRegistry.java
@@ -34,8 +34,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Collections;
 
-//import java.util.*;
-
 import static org.apache.ranger.plugin.policyengine.RangerPolicyEngine.GROUP_PUBLIC;
 
 public class RangerServiceSchemaRegistry extends RangerBaseService {
