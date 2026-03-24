e3628f0ddb614a776b31dd69b4c74e27089f4fb9
Ko-dany <dahyun.ko@acceldata.io>
2026-03-24 12:50:42 -0400

    Update TestServiceDBStore.java accordingly

--- security-admin/src/test/java/org/apache/ranger/biz/TestServiceDBStore.java
+++ security-admin/src/test/java/org/apache/ranger/biz/TestServiceDBStore.java
@@ -3025,8 +3025,8 @@ public void test47getMetricByTypeDenyconditions() throws Exception {
 		List<org.apache.ranger.s3.PolicyStatement> rangerStatements = java.util.Arrays.asList(rangerStmt);
 
 		// Execute
-		List<org.apache.ranger.s3.PolicyStatement> result = 
-			serviceDBStore.mergeWithIAMStatements(s3Client, "test-bucket", rangerStatements);
+		List<org.apache.ranger.s3.PolicyStatement> result =
+			serviceDBStore.mergeWithIAMStatements(s3Client, "test-bucket", rangerStatements, java.util.Collections.emptySet());
 
 		// Assert: only Ranger statements should be present
 		Assert.assertEquals(1, result.size());
@@ -3059,8 +3059,9 @@ public void test47getMetricByTypeDenyconditions() throws Exception {
 		List<org.apache.ranger.s3.PolicyStatement> rangerStatements = java.util.Arrays.asList(rangerStmt);
 
 		// Execute
-		List<org.apache.ranger.s3.PolicyStatement> result = 
-			serviceDBStore.mergeWithIAMStatements(s3Client, "test-bucket", rangerStatements);
+		java.util.Set<String> rangerManagedResources = new java.util.HashSet<>(java.util.Arrays.asList("arn:aws:s3:::ranger-bucket/*"));
+		List<org.apache.ranger.s3.PolicyStatement> result =
+			serviceDBStore.mergeWithIAMStatements(s3Client, "test-bucket", rangerStatements, rangerManagedResources);
 
 		// Assert: both IAM-only and Ranger statements should be present
 		Assert.assertEquals(2, result.size());
@@ -3096,8 +3097,9 @@ public void test47getMetricByTypeDenyconditions() throws Exception {
 		List<org.apache.ranger.s3.PolicyStatement> rangerStatements = java.util.Arrays.asList(rangerStmt);
 
 		// Execute
-		List<org.apache.ranger.s3.PolicyStatement> result = 
-			serviceDBStore.mergeWithIAMStatements(s3Client, "test-bucket", rangerStatements);
+		java.util.Set<String> rangerManagedResources = new java.util.HashSet<>(java.util.Arrays.asList("arn:aws:s3:::shared-bucket/*"));
+		List<org.apache.ranger.s3.PolicyStatement> result =
+			serviceDBStore.mergeWithIAMStatements(s3Client, "test-bucket", rangerStatements, rangerManagedResources);
 
 		// Assert: only Ranger statement should be present (replaces IAM statement)
 		Assert.assertEquals(1, result.size());
@@ -3142,7 +3144,7 @@ public void test47getMetricByTypeDenyconditions() throws Exception {
 			.thenReturn(software.amazon.awssdk.services.s3.model.PutBucketPolicyResponse.builder().build());
 
 		// Execute: processPolicies should process the bucket map
-		serviceDBStore.processPolicies(bucketMap, s3Client, iamClient);
+		serviceDBStore.processPolicies(bucketMap, s3Client, iamClient, java.util.Collections.emptySet());
 
 		// Assert: putBucketPolicy should be called once
 		Mockito.verify(s3Client, Mockito.times(1))
@@ -3204,7 +3206,7 @@ public void test47getMetricByTypeDenyconditions() throws Exception {
 			.thenReturn(software.amazon.awssdk.services.s3.model.PutBucketPolicyResponse.builder().build());
 
 		// Execute: processPolicies should process both buckets
-		serviceDBStore.processPolicies(bucketMap, s3Client, iamClient);
+		serviceDBStore.processPolicies(bucketMap, s3Client, iamClient, java.util.Collections.emptySet());
 
 		// Assert: putBucketPolicy should be called twice (once for each bucket)
 		Mockito.verify(s3Client, Mockito.times(2))
@@ -3285,7 +3287,7 @@ public void test47getMetricByTypeDenyconditions() throws Exception {
 			.thenReturn(software.amazon.awssdk.services.s3.model.PutBucketPolicyResponse.builder().build());
 
 		// Execute: processPolicies should handle both allow and deny items
-		serviceDBStore.processPolicies(bucketMap, s3Client, iamClient);
+		serviceDBStore.processPolicies(bucketMap, s3Client, iamClient, java.util.Collections.emptySet());
 
 		// Assert: putBucketPolicy should be called once
 		Mockito.verify(s3Client, Mockito.times(1))
@@ -3302,7 +3304,7 @@ public void test47getMetricByTypeDenyconditions() throws Exception {
 		software.amazon.awssdk.services.iam.IamClient iamClient = Mockito.mock(software.amazon.awssdk.services.iam.IamClient.class);
 
 		// Execute: processPolicies with empty map should not throw
-		serviceDBStore.processPolicies(bucketMap, s3Client, iamClient);
+		serviceDBStore.processPolicies(bucketMap, s3Client, iamClient, java.util.Collections.emptySet());
 
 		// Assert: putBucketPolicy should never be called
 		Mockito.verify(s3Client, Mockito.never())
