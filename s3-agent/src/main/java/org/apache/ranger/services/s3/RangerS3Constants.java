6051ea1371f76dcd45974a72a8f3dc5e84b6e31d
Dahyun (Dany) <88699158+ko-dany@users.noreply.github.com>
2026-04-01 16:55:27 -0400

    ODP-7209 | ODP-6188: Update Ranger S3 plugin to use custom IAM endpoint (#124)
    * ODP-6188 Update Ranger S3 plugin to build IAM Client to custom endpoint
    
    * ODP-6188 Added region in IAM Client provider for non-S3
    
    * ODP-6188 : Add integration tests for S3ClientConnectionMgr
    
    * ODP-6188 : Fix test failures
    
    * ODP-6188 Added config validation checks to avoid NPE errors. Delete one overlapping test case
    
    ---------
    
    Co-authored-by: dishtikundra <dishti@acceldata.io>
    Co-authored-by: Shubham Sharma <shubh.luck@yahoo.in>
    (cherry picked from commit ce45ff90400d9c5486a5e51edae9664d35e27308)

--- s3-agent/src/main/java/org/apache/ranger/services/s3/RangerS3Constants.java
+++ s3-agent/src/main/java/org/apache/ranger/services/s3/RangerS3Constants.java
@@ -25,6 +25,7 @@ public class RangerS3Constants {
 
     public static final String S3 = "s3";
     public static final String AWS = "AWS";
+    public static final String AWS_S3 = "aws_s3";
     public static final String ALLOW = "Allow";
     public static final String DENY = "Deny";
     public static final String S3_POLICY_LANGUAGE_VERSION = "2012-10-17";
