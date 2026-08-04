300441d36d40c83dc11852d44bb83e867375695a
Ko-dany <dahyun.ko@acceldata.io>
2026-08-04 15:14:22 -0400

    ODP-6879: Update ODP versions for ODP 3.4.3.0-1

--- hive-agent/src/main/java/org/apache/ranger/authorization/hive/authorizer/RangerHiveAuthorizerBase.java
+++ hive-agent/src/main/java/org/apache/ranger/authorization/hive/authorizer/RangerHiveAuthorizerBase.java
@@ -20,6 +20,7 @@
 package org.apache.ranger.authorization.hive.authorizer;
 
 import org.apache.hadoop.hive.conf.HiveConf;
+import org.apache.hadoop.hive.conf.HiveConf.ConfVars;
 import org.apache.hadoop.hive.ql.security.HiveAuthenticationProvider;
 import org.apache.hadoop.hive.ql.security.authorization.plugin.AbstractHiveAuthorizer;
 import org.apache.hadoop.hive.ql.security.authorization.plugin.DisallowTransformHook;
@@ -115,16 +116,15 @@ public abstract class RangerHiveAuthorizerBase extends AbstractHiveAuthorizer {
 
         // from SQLStdHiveAccessController.applyAuthorizationConfigPolicy()
         if (mSessionContext != null && mSessionContext.getClientType() == CLIENT_TYPE.HIVESERVER2) {
-            // Configure PREEXECHOOKS with DisallowTransformHook to disallow transform queries
-            String hooks = hiveConf.getVar(HiveConf.getConfVars("hive.exec.pre.hooks")).trim();
-
+            // Configure PRE_EXEC_HOOKS with DisallowTransformHook to disallow transform queries
+            String hooks = hiveConf.getVar(ConfVars.PRE_EXEC_HOOKS).trim();
             if (hooks.isEmpty()) {
                 hooks = DisallowTransformHook.class.getName();
             } else {
                 hooks = hooks + "," + DisallowTransformHook.class.getName();
             }
 
-            hiveConf.setVar(HiveConf.getConfVars("hive.exec.pre.hooks"), hooks);
+            hiveConf.setVar(ConfVars.PRE_EXEC_HOOKS, hooks);
 
             SettableConfigUpdater.setHiveConfWhiteList(hiveConf);
         }
