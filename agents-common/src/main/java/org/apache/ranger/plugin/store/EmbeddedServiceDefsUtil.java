a4cfe0f453b4f30f1357dfc81814010225aab695
dishtikundra <dishti@acceldata.io>
2026-08-24 14:10:29 +0530

    ODP-6600 Revert Yunikorn and polaris servicedef references

--- agents-common/src/main/java/org/apache/ranger/plugin/store/EmbeddedServiceDefsUtil.java
+++ agents-common/src/main/java/org/apache/ranger/plugin/store/EmbeddedServiceDefsUtil.java
@@ -80,6 +80,8 @@ public class EmbeddedServiceDefsUtil {
 	public static final String EMBEDDED_SERVICEDEF_NESTEDSTRUCTURE_NAME  = "nestedstructure";
 	public static final String EMBEDDED_SERVICEDEF_S3_NAME  = "s3";
 	public static final String EMBEDDED_SERVICEDEF_GCS_NAME = "gcs";
+	public static final String EMBEDDED_SERVICEDEF_YUNIKORN_NAME = "yunikorn";
+	public static final String EMBEDDED_SERVICEDEF_POLARIS_NAME = "polaris";
 
 	public static final String PROPERTY_CREATE_EMBEDDED_SERVICE_DEFS = "ranger.service.store.create.embedded.service-defs";
 
@@ -129,6 +131,8 @@ public class EmbeddedServiceDefsUtil {
 	private RangerServiceDef kuduServiceDef;
 	private RangerServiceDef nestedStructureServiveDef;
 	private RangerServiceDef s3ServiceDef;
+	private RangerServiceDef yuniKornServiceDef;
+	private RangerServiceDef polarisServiceDef;
 	private RangerServiceDef gcsServiceDef;
 
 	private RangerServiceDef tagServiceDef;
@@ -181,7 +185,8 @@ public class EmbeddedServiceDefsUtil {
 			nestedStructureServiveDef = getOrCreateServiceDef(store, EMBEDDED_SERVICEDEF_NESTEDSTRUCTURE_NAME);
 			s3ServiceDef = getOrCreateServiceDef(store, EMBEDDED_SERVICEDEF_S3_NAME);
 			gcsServiceDef = getOrCreateServiceDef(store, EMBEDDED_SERVICEDEF_GCS_NAME);
-
+			yuniKornServiceDef = getOrCreateServiceDef(store, EMBEDDED_SERVICEDEF_YUNIKORN_NAME);
+			polarisServiceDef = getOrCreateServiceDef(store, EMBEDDED_SERVICEDEF_POLARIS_NAME);
 			tagServiceDef = getOrCreateServiceDef(store, EMBEDDED_SERVICEDEF_TAG_NAME);
 			gdsServiceDef = getOrCreateServiceDef(store, EMBEDDED_SERVICEDEF_GDS_NAME);
 
@@ -275,6 +280,10 @@ public class EmbeddedServiceDefsUtil {
 	public long getNestedStructureServiceDefId() { return getId(nestedStructureServiveDef); }
 
 	public long getS3ServiceDefId() { return getId(s3ServiceDef); }
+	
+	public long getYuniKornServiceDefId() { return getId(yuniKornServiceDef); }
+
+	public long getPolarisServiceDefId() { return getId(polarisServiceDef); }
 
 	public long getGCSServiceDefId() { return getId(gcsServiceDef); }
 
