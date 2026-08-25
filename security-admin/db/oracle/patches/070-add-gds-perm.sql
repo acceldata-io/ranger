b1cc674afc26841be1a61ad2db1435f13fa1d3f2
sehaj-sandhu <110890721+sehaj-sandhu@users.noreply.github.com>
2026-08-25 16:18:14 +0530

    RANGER-5180: GDS feature support on Oracle database (#196)

--- security-admin/db/oracle/patches/070-add-gds-perm.sql
+++ security-admin/db/oracle/patches/070-add-gds-perm.sql
@@ -13,39 +13,52 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
-DECLARE
-    v_count NUMBER := 0;
-    v_admin_id NUMBER := 0;
-    v_usersync_id NUMBER := 0;
-    v_tagsync_id NUMBER := 0;
-    v_module_id NUMBER := 0;
+CREATE OR REPLACE FUNCTION getXportalUIdByLoginId(input_val IN VARCHAR2)
+RETURN NUMBER iS
 BEGIN
-    SELECT id INTO v_admin_id FROM x_portal_user WHERE login_id = 'admin';
-    BEGIN SELECT id INTO v_usersync_id FROM x_portal_user WHERE login_id = 'rangerusersync'; EXCEPTION WHEN NO_DATA_FOUND THEN v_usersync_id := 0; END;
-    BEGIN SELECT id INTO v_tagsync_id FROM x_portal_user WHERE login_id = 'rangertagsync'; EXCEPTION WHEN NO_DATA_FOUND THEN v_tagsync_id := 0; END;
-    SELECT COUNT(*) INTO v_count FROM x_modules_master WHERE module = 'Governed Data Sharing';
-    IF v_count = 0 THEN
-        INSERT INTO x_modules_master VALUES(X_MODULES_MASTER_SEQ.NEXTVAL, SYSDATE, SYSDATE, v_admin_id, v_admin_id, 'Governed Data Sharing', '');
-    END IF;
-    SELECT id INTO v_module_id FROM x_modules_master WHERE module = 'Governed Data Sharing';
-    IF v_admin_id > 0 AND v_module_id > 0 THEN
-        SELECT COUNT(*) INTO v_count FROM x_user_module_perm WHERE user_id = v_admin_id AND module_id = v_module_id;
-        IF v_count = 0 THEN
-            INSERT INTO x_user_module_perm (id,user_id,module_id,create_time,update_time,added_by_id,upd_by_id,is_allowed) VALUES (X_USER_MODULE_PERM_SEQ.NEXTVAL,v_admin_id,v_module_id,SYSDATE,SYSDATE,v_admin_id,v_admin_id,1);
-        END IF;
-    END IF;
-    IF v_usersync_id > 0 AND v_module_id > 0 THEN
-        SELECT COUNT(*) INTO v_count FROM x_user_module_perm WHERE user_id = v_usersync_id AND module_id = v_module_id;
-        IF v_count = 0 THEN
-            INSERT INTO x_user_module_perm (id,user_id,module_id,create_time,update_time,added_by_id,upd_by_id,is_allowed) VALUES (X_USER_MODULE_PERM_SEQ.NEXTVAL,v_usersync_id,v_module_id,SYSDATE,SYSDATE,v_admin_id,v_admin_id,1);
-        END IF;
-    END IF;
-    IF v_tagsync_id > 0 AND v_module_id > 0 THEN
-        SELECT COUNT(*) INTO v_count FROM x_user_module_perm WHERE user_id = v_tagsync_id AND module_id = v_module_id;
-        IF v_count = 0 THEN
-            INSERT INTO x_user_module_perm (id,user_id,module_id,create_time,update_time,added_by_id,upd_by_id,is_allowed) VALUES (X_USER_MODULE_PERM_SEQ.NEXTVAL,v_tagsync_id,v_module_id,SYSDATE,SYSDATE,v_admin_id,v_admin_id,1);
-        END IF;
-    END IF;
-    COMMIT;
-END;
-/
+DECLARE
+myid Number := 0;
+begin
+    SELECT x_portal_user.id into myid FROM x_portal_user
+    WHERE x_portal_user.login_id=input_val;
+    RETURN myid;
+end;
+END;/
+
+
+CREATE OR REPLACE FUNCTION getModulesIdByName(inputval IN VARCHAR2)
+RETURN NUMBER is
+BEGIN
+Declare
+myid Number := 0;
+begin
+   SELECT id into myid FROM x_modules_master
+   WHERE MODULE = inputval;
+   RETURN myid;
+end;
+END;/
+
+DECLARE
+        v_count number:=0;
+BEGIN
+        select count(*) into v_count from x_modules_master where module='Governed Data Sharing';
+        if (v_count = 0) then
+            INSERT INTO x_modules_master VALUES(X_MODULES_MASTER_SEQ.NEXTVAL,sys_extract_utc(systimestamp),sys_extract_utc(systimestamp),getXportalUIdByLoginId('admin'),getXportalUIdByLoginId('admin'),'Governed Data Sharing','');
+        end if;
+        v_count:=0;
+        select count(*) into v_count from x_user_module_perm where user_id=getXportalUIdByLoginId('admin') and module_id=getModulesIdByName('Governed Data Sharing');
+        if (v_count = 0) then
+            INSERT INTO x_user_module_perm (id,user_id,module_id,create_time,update_time,added_by_id,upd_by_id,is_allowed) VALUES (X_USER_MODULE_PERM_SEQ.nextval,getXportalUIdByLoginId('admin'),getModulesIdByName('Governed Data Sharing'),sys_extract_utc(systimestamp),sys_extract_utc(systimestamp),getXportalUIdByLoginId('admin'),getXportalUIdByLoginId('admin'),1);
+        end if;
+        v_count:=0;
+        select count(*) into v_count from x_user_module_perm where user_id=getXportalUIdByLoginId('rangerusersync') and module_id=getModulesIdByName('Governed Data Sharing');
+        if (v_count = 0) then
+            INSERT INTO x_user_module_perm (id,user_id,module_id,create_time,update_time,added_by_id,upd_by_id,is_allowed) VALUES (X_USER_MODULE_PERM_SEQ.nextval,getXportalUIdByLoginId('rangerusersync'),getModulesIdByName('Governed Data Sharing'),sys_extract_utc(systimestamp),sys_extract_utc(systimestamp),getXportalUIdByLoginId('admin'),getXportalUIdByLoginId('admin'),1);
+        end if;
+        v_count:=0;
+        select count(*) into v_count from x_user_module_perm where user_id=getXportalUIdByLoginId('rangertagsync') and module_id=getModulesIdByName('Governed Data Sharing');
+        if (v_count = 0) then
+            INSERT INTO x_user_module_perm (id,user_id,module_id,create_time,update_time,added_by_id,upd_by_id,is_allowed) VALUES (X_USER_MODULE_PERM_SEQ.nextval,getXportalUIdByLoginId('rangertagsync'),getModulesIdByName('Governed Data Sharing'),sys_extract_utc(systimestamp),sys_extract_utc(systimestamp),getXportalUIdByLoginId('admin'),getXportalUIdByLoginId('admin'),1);
+        end if;
+        commit;
+END;/
--- security-admin/db/oracle/patches/071-alter-x_service_version_info.sql
+++ security-admin/db/oracle/patches/071-alter-x_service_version_info.sql
@@ -21,16 +21,14 @@ BEGIN
                 execute immediate 'ALTER TABLE x_service_version_info ADD gds_version NUMBER(20) DEFAULT 0 NOT NULL';
         end if;
         commit;
-END;
-/
+END;/
 
 DECLARE
-        v_count number:=0;
+        v_column_exists number:=0;
 BEGIN
-        select count(*) into v_count from user_tab_cols where table_name='X_SERVICE_VERSION_INFO' and column_name='GDS_UPDATE_TIME';
-        if (v_count = 0) then
+        select count(*) into v_column_exists from user_tab_cols where table_name='X_SERVICE_VERSION_INFO' and column_name='GDS_UPDATE_TIME';
+        if (v_column_exists = 0) then
                 execute immediate 'ALTER TABLE x_service_version_info ADD gds_update_time DATE DEFAULT NULL NULL';
         end if;
         commit;
-END;
-/
+END;/
